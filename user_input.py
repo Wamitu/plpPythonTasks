f3d2a4de00e43fd2ca8531fa5d80bb6f7a7fb1c3
Wamitu <90007148+Wamitu@users.noreply.github.com>
2024-03-16 07:54:26 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,10 @@
+name=input("What is your name?")
+print(name)
+
+age=input("What is your age?")
+print(age)
+
+location=input("What is your location?")
+print(location)
+
+print("Hello " +name +", "+"you are " +age +" years old, and live in " +location  )
